7640f072074b7e70799c154f982141a937fb0e9a
Nihaal Sangha <nihaal.s@live.co.uk>
2019-01-18 18:30:21 +0000

    Updated

--- main.js
+++ main.js
@@ -1,44 +1,49 @@
 $.ajax({
     'url': '//data.iana.org/TLD/tlds-alpha-by-domain.txt',
     'dataType': 'plain'
-}).always((data, a, b) => {
-    // console.log(data.responseText);
+}).always((data) => {
     setList(data.responseText);
     setupSearch();
 });
 
 function setList(requestText) {
-    var tlds = requestText.split('\n');
-    tlds.shift();
+    let tlds = requestText.split('\n');
+    const dataFromFull = tlds.shift();
 
-    fragment = document.createDocumentFragment();
+    const regex = /# Version \d+, Last Updated ([a-zA-Z 0-9:]+)/;
+
+    const dataFrom = `Data from ${regex.exec(dataFromFull)[1]}`;
+
+    $('#data-from').text(dataFrom);
+
+    const fragment = document.createDocumentFragment();
     for (let tld of tlds) {
-        // name = name.toLowerCase();
         const li = document.createElement('li');
         li.textContent = tld;
         fragment.appendChild(li);
     }
 
-    const container = document.getElementById('live-search-list');
+    const container = $('#live-search-list')[0];
     container.appendChild(fragment);
 }
 
 function setupSearch() {
-    $('.live-search-list li').each(function () {
-        $(this).attr('data-search-term', $(this).text().toLowerCase());
+    $('.live-search-list li').each((index, elem) => {
+        elem = $(elem);
+        elem.attr('data-search-term', elem.text().toLowerCase());
     });
 
-    $('.live-search-box').on('keyup', function () {
-        var searchTerm = $(this).val().toLowerCase();
+    $('.live-search-box').keyup((event) => {
+        const inputBox = event.target;
+        const searchTerm = inputBox.value.toLowerCase();
 
-        $('.live-search-list li').each(function () {
-            if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
-                $(this).show();
+        $('.live-search-list li').each((index, elem) => {
+            elem = $(elem);
+            if (elem.filter(`[data-search-term *= ${searchTerm}]`).length > 0 || searchTerm.length < 1) {
+                elem.show();
             } else {
-                $(this).hide();
+                elem.hide();
             }
-
         });
-
     });
 }
